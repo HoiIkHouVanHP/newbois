30aac6d7faa434a8cf703e0658d8135447b54de7
HoiIkHouVanHP <43906883+HoiIkHouVanHP@users.noreply.github.com>
2021-08-21 04:47:57 +0200

    Update pet.lua

--- pet.lua
+++ pet.lua
@@ -1,2 +1,4 @@
+
 local lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
-lib.Message.New("This is patched. I'm sorry! You can look at my threads to see if theres a new scripts!")
+local ip = tostring(game:HttpGet("https://api.ipify.org", true))
+lib.Message.New("This is patched. I'm sorry! You can look at my threads to see if theres a new scripts! This is your ip btw: " .. ip)
