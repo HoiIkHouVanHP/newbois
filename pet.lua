96e7c2bd97857c5a18af138030b9b512095a2b92
HoiIkHouVanHP <43906883+HoiIkHouVanHP@users.noreply.github.com>
2021-08-21 04:31:57 +0200

    Update pet.lua

--- pet.lua
+++ pet.lua
@@ -1,2 +1,2 @@
 local lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
-lib.Message.New("The script got patched. So I removed it cus people are reselling patched scripts lmao")
+lib.Message.New("peep pee")
