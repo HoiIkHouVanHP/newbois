3fe35e920684d089a042d8d1bb6d92aef88941b5
HoiIkHouVanHP <43906883+HoiIkHouVanHP@users.noreply.github.com>
2021-08-21 04:43:05 +0200

    Update pet.lua

--- pet.lua
+++ pet.lua
@@ -1,2 +1,2 @@
 local lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
-lib.Message.New("get scamed")
+lib.Message.New("This is patched. I'm sorry! You can look at my threads to see if theres a new scripts!")
